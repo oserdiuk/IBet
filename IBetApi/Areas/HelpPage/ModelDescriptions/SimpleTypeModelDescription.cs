7e59565f45f39d7dc5645808e12eb2ce28bcfe52
Oksana <oserdiuk@outlook.com>
2015-12-25 23:03:13 +0200

    fixed errors with api

--- IBetApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace IBetApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- IBetApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IBetApi.WebApiApplication" Language="C#" %>
