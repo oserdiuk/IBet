92be0f2bcd957faff9a49f4cad423b42e1d4e64f
Oksana <oserdiuk@outlook.com>
2015-12-25 23:18:42 +0200

    done web api config

--- IBetApp/Global.asax.cs
+++ IBetApp/Global.asax.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -17,10 +18,10 @@ namespace IBetApp
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AutoMapperStartup.InitializeMapper();
-
 
         }
     }
